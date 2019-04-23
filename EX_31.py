8638dc42261fa937c119e79226f4621102c5a471
szora <szorakozza@gmail.com>
2019-04-23 20:27:46 +0200

    a

--- EX_31.py
+++ EX_31.py
@@ -5,11 +5,12 @@ def felhasznalo(n):
         k=n.find("@")
         fh=n[:k]
         a=string.ascii_letters + string.digits
-        if (fh[0] in a) and (fh[-1] in a):
-            a+="-"+"_"+"."
-            for i in range(len(fh)):
-                if (fh[i]=="." and (fh[i+1]=="." or fh[i-1]==".")) or (fh[i] not in a):
-                    return False
+        if len(fh)>1:
+            if (fh[0] in a) and (fh[-1] in a):
+                a+="-"+"_"+"."
+                for i in range(len(fh)):
+                    if (fh[i]=="." and (fh[i+1]=="." or fh[i-1]==".")) or (fh[i] not in a):
+                        return False
     return True
 
 def kiszolgalo(n):
@@ -18,12 +19,13 @@ def kiszolgalo(n):
         sz=n[:-3].rfind(".")
         ksz=n[k+1:sz]
         a=string.ascii_letters + string.digits
-        if (ksz[0] in a) and (ksz[-1] in a):
-            a+="-"+"."
-            for i in range(len(ksz)):
-                if (ksz[i]=="." and (ksz[i+1]=="." or ksz[i-1]==".")) or (ksz[i] not in a):
-                    return False
-        return True
+        if len(ksz)>1:
+            if (ksz[0] in a) and (ksz[-1] in a):
+                a+="-"+"."
+                for i in range(len(ksz)):
+                    if (ksz[i]=="." and (ksz[i+1]=="." or ksz[i-1]==".")) or (ksz[i] not in a):
+                        return False
+            return True
     return False
 
 def tld(n):
